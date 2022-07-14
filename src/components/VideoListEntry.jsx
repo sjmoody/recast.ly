0b96124c191431a244dcf8458a09ae42493d068d
hray <101483930+bubsinthemountains@users.noreply.github.com>
2022-07-13 23:48:51 -0400

    Refactored App.jsx

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,4 +1,5 @@
 import exampleVideoData from '../data/exampleVideoData.js';
+import App from './App.js';
 
 var VideoListEntry = (props) => (
   <div className="video-list-entry media">
@@ -6,7 +7,7 @@ var VideoListEntry = (props) => (
       <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">{props.video.snippet.title}</div>
+      <div className="video-list-entry-title" onClick={onVideoListEntryClick(props.video)}>{props.video.snippet.title}</div>
       <div className="video-list-entry-detail">{props.video.snippet.description}</div>
     </div>
   </div>
