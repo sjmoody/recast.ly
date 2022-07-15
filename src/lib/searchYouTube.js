e0023457d81d01f7a661235a6615e0c721312fcd
Steven Moody <smoody07@gmail.com>
2022-07-14 19:51:09 -0700

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -6,6 +6,19 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
 
 var searchYouTube = (query, callback) => {
   // TODO
+  // Endpoint: https://app-hrsei-api.herokuapp.com/api/recastly/videos
+  // Use jQuery to send a GET request to the search endpoint. This is the only time you should use jQuery in this sprint
+  // Accept a callback function that is invoked with the videos array that is returned from hitting the endpoint
+  // Accept an q string to search for
+  let endpoint = 'https://app-hrsei-api.herokuapp.com/api/recastly/videos';
+  // var url = Endpoint + query;
+  $.get({
+    url: endpoint,
+    data: {q: query},
+    success: callback
+  });
+
 };
 
 export default searchYouTube;
+
